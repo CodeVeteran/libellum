cb6d511cd82797f90e4df511316ef87ac64bcd49
Alex Caron <alexcaron.coder@outlook.com>
2019-09-10 20:32:28 -0400

    Login Frontend
    Include libs (jQuery, bootstrap) and made the front end of the login page.

--- index.php
+++ index.php
@@ -1,5 +1,37 @@
-<?php
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Login - Libellum</title>
+        <link rel="stylesheet" href="/lib/bootstrap4.3.1/css/bootstrap.min.css">
+        <link rel="stylesheet" href="/assets/css/login.css">
+        <script src="/lib/jquery3.4.1/jquery.min.js"></script>
+        <script src="/lib/bootstrap4.3.1/js/bootstrap.min.js"></script>
+    <head>
+    <body>
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-9 col-md-7 col-lg-5 mx-auto">
+                    <div class="card card-signin my-5">
+                        <div class="card-body">
+                            <h5 class="card-title text-center">Sign In</h5>
+                            <form class="form-signin">
+                                <div class="form-label-group">
+                                    <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
+                                    <label for="inputEmail">Email address</label>
+                                </div>
 
-echo 'test';
+                                <div class="form-label-group">
+                                    <input type="password" id="inputPassword" class="form-control" placeholder="Password" required>
+                                    <label for="inputPassword">Password</label>
+                                </div>
 
-?>
+                                <button class="btn btn-lg btn-primary btn-block text-uppercase" type="submit">Sign in</button>
+                            
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </body>
+</html
